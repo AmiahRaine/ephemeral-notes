7eb0250573b2095f826bafaecd851f9a22ea43ee
AmiahRaine <amiah.belleisle@proton.me>
2025-11-30 19:04:09 -0600

    initial commit

---
+++ app/src/main/java/dev/amiah/ephemeral/MainActivity.kt
@@ -0,0 +1,52 @@
+package dev.amiah.ephemeral
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.amiah.ephemeral.ui.theme.EphemeralTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EphemeralTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Slider() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EphemeralTheme {
+        Greeting("Android")
+    }
+}
