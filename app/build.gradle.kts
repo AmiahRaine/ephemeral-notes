0ea02622e30b22cfe46b99aaa97878f5281d0986
AmiahRaine <amiah.belleisle@proton.me>
2025-12-18 20:03:24 -0600

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.navigation.compose)
+    implementation(libs.androidx.junit.ktx)
     ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
