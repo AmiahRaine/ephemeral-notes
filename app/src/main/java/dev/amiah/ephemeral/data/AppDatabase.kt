de1087f1e90738dc712f7b7a64e0df9216fa5b0c
AmiahRaine <amiah.belleisle@proton.me>
2025-12-28 18:22:02 -0600

    Update db

--- app/src/main/java/dev/amiah/ephemeral/data/AppDatabase.kt
+++ app/src/main/java/dev/amiah/ephemeral/data/AppDatabase.kt
@@ -9,7 +9,7 @@ import dev.amiah.ephemeral.data.entity.LongTermNote
 import dev.amiah.ephemeral.data.entity.Note
 import dev.amiah.ephemeral.data.entity.Task
 
-@Database(entities = [Note::class, Task::class, LongTermNote::class], version = 3)
+@Database(entities = [Note::class, Task::class, LongTermNote::class], version = 4)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
--- app/src/main/java/dev/amiah/ephemeral/data/Converters.kt
+++ app/src/main/java/dev/amiah/ephemeral/data/Converters.kt
@@ -1,21 +1,9 @@
 package dev.amiah.ephemeral.data
 
 import androidx.room.TypeConverter
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
 import java.time.Instant
 
 class Converters {
-
-    @TypeConverter
-    fun toDate(dateString: String): ZonedDateTime {
-        return ZonedDateTime.parse(dateString);
-    }
-
-    @TypeConverter
-    fun fromDate(date: ZonedDateTime): String {
-        return date.format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
-    }
 
     @TypeConverter
     fun toInstant(instantString: String): Instant {
@@ -26,6 +14,5 @@ class Converters {
     fun fromInstant(instant: Instant): String {
         return instant.toString();
     }
-
 
 }
