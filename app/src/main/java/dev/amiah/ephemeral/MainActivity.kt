15cc1c4c59bfdd4260052e5614aab4cedc919270
AmiahRaine <amiah.belleisle@proton.me>
2026-01-01 14:01:07 -0600

    Clear focus when clicking off of text fields

--- app/src/main/java/dev/amiah/ephemeral/MainActivity.kt
+++ app/src/main/java/dev/amiah/ephemeral/MainActivity.kt
@@ -5,16 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -22,6 +27,7 @@ import androidx.room.Room
 import dev.amiah.ephemeral.data.AppDatabase
 import dev.amiah.ephemeral.ui.screen.NoteSlider
 import dev.amiah.ephemeral.ui.theme.EphemeralTheme
+import dev.amiah.ephemeral.viewmodel.note.NotesEvent
 import dev.amiah.ephemeral.viewmodel.note.NotesViewModel
 
 class MainActivity : ComponentActivity() {
@@ -55,13 +61,24 @@ class MainActivity : ComponentActivity() {
         setContent {
 
             val state by viewModel.state.collectAsState(null)
+            val focusManager = LocalFocusManager.current
 
             EphemeralTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()
+                    .pointerInput(Unit) {
+                        detectTapGestures {
+                            // Make text fields loose focus when clicking off of them
+                            focusManager.clearFocus();
+                            // Set current task to null, since no text fields are currently selected
+                            viewModel.onNoteEvent(NotesEvent.SwitchCurrentTask(null))
+                        }
+                    }
+                ) { innerPadding ->
                     Column(Modifier.padding(innerPadding)) {
 
                         // This needs its own state
                         Box(modifier = Modifier.weight(0.25f)) {
+                            TextField(rememberTextFieldState())
                             //NoteSlider(state, viewModel::onNoteEvent)
                         }
                         // This is the regular one that the state was made for
