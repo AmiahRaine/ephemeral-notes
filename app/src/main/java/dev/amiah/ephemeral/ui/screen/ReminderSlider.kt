68352d633002c194dc506c163fa925112a9d56a7
AmiahRaine <amiah.belleisle@proton.me>
2026-01-03 15:44:39 -0600

    Created

---
+++ app/src/main/java/dev/amiah/ephemeral/ui/screen/ReminderSlider.kt
@@ -0,0 +1,11 @@
+package dev.amiah.ephemeral.ui.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import dev.amiah.ephemeral.viewmodel.longtermnote.ReminderEvent
+import dev.amiah.ephemeral.viewmodel.longtermnote.ReminderState
+
+@Composable
+fun ReminderSlider(reminderState: ReminderState?, onEvent: (ReminderEvent) -> Unit) {
+    Text("TEST")
+}
