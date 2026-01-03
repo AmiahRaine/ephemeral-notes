3090e57e0ebcc7febfd0a0edac9c9feaea6bbc21
AmiahRaine <amiah.belleisle@proton.me>
2026-01-03 15:45:06 -0600

    Added view model basics

--- app/src/main/java/dev/amiah/ephemeral/viewmodel/longtermnote/ReminderViewModel.kt
+++ app/src/main/java/dev/amiah/ephemeral/viewmodel/longtermnote/ReminderViewModel.kt
@@ -1,12 +1,43 @@
 package dev.amiah.ephemeral.viewmodel.longtermnote
 
-import dev.amiah.ephemeral.data.dao.NoteDao
-import dev.amiah.ephemeral.data.dao.TaskDao
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.amiah.ephemeral.data.dao.ReminderDao
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import java.time.Instant
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class ReminderViewModel(private val noteDao: NoteDao, private val taskDao: TaskDao) {
+class ReminderViewModel(private val reminderDao: ReminderDao) : ViewModel() {
 
+    // Default ReminderState.
+    private val _state = MutableStateFlow(ReminderState())
 
+    private val _reminders = reminderDao.getActiveRemindersByDate(Instant.now())
+
+    val state = combine(_state, _reminders) { state, reminders ->
+        state.copy(
+            reminders = reminders
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), ReminderState())
+
+    // Used to debounce saving the text.
+    private var _saveTextJob: Job? = null;
+
+    fun onReminderEvent(event: ReminderEvent) {
+        when (event) {
+            ReminderEvent.CreateReminder -> TODO()
+            is ReminderEvent.DeleteReminder -> TODO()
+            is ReminderEvent.DeleteTask -> TODO()
+            is ReminderEvent.ModifyReminderTextFieldValue -> TODO()
+            is ReminderEvent.SaveReminder -> TODO()
+            is ReminderEvent.SaveReminderText -> TODO()
+            is ReminderEvent.SwitchCurrentTask -> TODO()
+        }
+    }
 
 }
