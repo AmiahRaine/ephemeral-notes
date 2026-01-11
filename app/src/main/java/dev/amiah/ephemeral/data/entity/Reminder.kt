aa472173e0a6bd2ce43baf097fcebe59ab61a0bc
AmiahRaine <amiah.belleisle@proton.me>
2026-01-10 18:55:35 -0600

    Default value for time field

--- app/src/main/java/dev/amiah/ephemeral/data/entity/Reminder.kt
+++ app/src/main/java/dev/amiah/ephemeral/data/entity/Reminder.kt
@@ -8,7 +8,7 @@ import java.time.Instant
 @Entity
 data class Reminder (
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
-    @ColumnInfo(name = "time") val time: Instant,
+    @ColumnInfo(name = "time") val time: Instant = Instant.MIN,
     @ColumnInfo(name = "text") val text: String = ""
 
 )
