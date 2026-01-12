70f6747da3b0b8d7845b247799b97212b94b0870
AmiahRaine <amiah.belleisle@proton.me>
2026-01-12 00:27:58 -0600

    Rename + secondary constructor

--- app/src/main/java/dev/amiah/ephemeral/viewmodel/longtermnote/ReminderEvent.kt
+++ app/src/main/java/dev/amiah/ephemeral/viewmodel/longtermnote/ReminderEvent.kt
@@ -11,7 +11,9 @@ sealed interface ReminderEvent {
     data class SaveReminderWithDebounce(val reminder: Reminder): ReminderEvent
     data class DeleteReminder(val reminder: Reminder): ReminderEvent
 
-    data class ModifyReminderTextFieldValue(val textValue: TextFieldValue): ReminderEvent
+    data class SetReminderTextFieldValue(val textValue: TextFieldValue): ReminderEvent {
+        constructor(text: String): this(TextFieldValue(text))
+    }
 
     data class SetDateTimeModalVisibility(val show: Boolean): ReminderEvent
 
