d5f6ccafc897a9ab863351c358442d75d9cc1915
AmiahRaine <amiah.belleisle@proton.me>
2026-01-11 21:34:55 -0600

    Refactored events

--- app/src/main/java/dev/amiah/ephemeral/viewmodel/longtermnote/ReminderEvent.kt
+++ app/src/main/java/dev/amiah/ephemeral/viewmodel/longtermnote/ReminderEvent.kt
@@ -2,22 +2,16 @@ package dev.amiah.ephemeral.viewmodel.longtermnote
 
 import androidx.compose.ui.text.input.TextFieldValue
 import dev.amiah.ephemeral.data.entity.Reminder
-import dev.amiah.ephemeral.data.entity.Task
 import java.time.LocalDateTime
 
 sealed interface ReminderEvent {
 
+    data class CreateReminder(val dateTime: LocalDateTime): ReminderEvent
     data class SaveReminder(val reminder: Reminder): ReminderEvent
+    data class SaveReminderWithDebounce(val reminder: Reminder): ReminderEvent
     data class DeleteReminder(val reminder: Reminder): ReminderEvent
 
-    data class SaveReminderText(val textFieldValue: TextFieldValue): ReminderEvent {
-        constructor(text: String): this(TextFieldValue(text = text))
-    }
     data class ModifyReminderTextFieldValue(val textValue: TextFieldValue): ReminderEvent
-
-    data class CreateReminder(val dateTime: LocalDateTime): ReminderEvent
-    data class SwitchCurrentTask(val task: Task?): ReminderEvent
-    data class DeleteTask(val task: Task): ReminderEvent
 
     data class SetDateTimeModalVisibility(val show: Boolean): ReminderEvent
 
