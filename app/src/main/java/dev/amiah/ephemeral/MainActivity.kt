caf122b8a8e5504ffe8ef06d4cbb0dfaf614847d
AmiahRaine <amiah.belleisle@proton.me>
2026-01-16 21:55:20 -0600

    Add Navigation

--- app/src/main/java/dev/amiah/ephemeral/MainActivity.kt
+++ app/src/main/java/dev/amiah/ephemeral/MainActivity.kt
@@ -11,29 +11,43 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
 import dev.amiah.ephemeral.data.AppDatabase
 import dev.amiah.ephemeral.ui.element.ActionBar
 import dev.amiah.ephemeral.ui.element.NoteSlider
 import dev.amiah.ephemeral.ui.element.ReminderSlider
 import dev.amiah.ephemeral.ui.theme.EphemeralTheme
+import dev.amiah.ephemeral.viewmodel.longtermnote.RemindersState
 import dev.amiah.ephemeral.viewmodel.longtermnote.RemindersViewModel
 import dev.amiah.ephemeral.viewmodel.note.NoteEvent
+import dev.amiah.ephemeral.viewmodel.note.NotesState
 import dev.amiah.ephemeral.viewmodel.note.NotesViewModel
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
 
-    lateinit var db: AppDatabase;
+    @Serializable
+    object MainScreen
+    @Serializable
+    object ViewReminders
+    @Serializable
+    object ViewTasks
+    @Serializable
+    object Settings
+
+    lateinit var db: AppDatabase
 
     private val notesViewModel by viewModels<NotesViewModel>(
         factoryProducer = {
@@ -71,6 +85,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
 
+            val navController = rememberNavController()
             val notesState by notesViewModel.state.collectAsState(null)
             val remindersState by remindersViewModel.state.collectAsState(null)
             val focusManager = LocalFocusManager.current
@@ -88,17 +103,23 @@ class MainActivity : ComponentActivity() {
                 ) { innerPadding ->
                     Column(Modifier.padding(innerPadding)) {
 
-                        Box() {
-                            ActionBar()
+                        NavHost(navController, startDestination = MainScreen) {
+                            composable<MainScreen> {
+                                MainScreen(
+                                    remindersState, remindersViewModel,
+                                    notesState, notesViewModel, navController
+                                )
+                            }
+
+                            composable<ViewReminders> {  }
+
+                            composable<ViewReminders> {  }
+
+                            composable<Settings> {  }
+
                         }
 
-                        Box(modifier = Modifier.weight(0.25f)) {
-                            ReminderSlider(remindersState, remindersViewModel::onReminderEvent)
-                        }
 
-                        Box(modifier = Modifier.weight(1f)) {
-                            NoteSlider(notesState, notesViewModel::onNoteEvent)
-                        }
 
                     }
                 }
@@ -107,18 +128,26 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/**
+ * Primary screen of the app
+ */
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen(
+    remindersState: RemindersState?, remindersViewModel: RemindersViewModel,
+    notesState: NotesState?, notesViewModel: NotesViewModel,
+    navController: NavHostController
+) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box() {
+            ActionBar(navController)
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EphemeralTheme {
-        Greeting("Android")
+        Box(modifier = Modifier.weight(0.25f)) {
+            ReminderSlider(remindersState, remindersViewModel::onReminderEvent)
+        }
+
+        Box(modifier = Modifier.weight(1f)) {
+            NoteSlider(notesState, notesViewModel::onNoteEvent)
+        }
     }
 }
--- app/src/main/java/dev/amiah/ephemeral/ui/element/ActionBar.kt
+++ app/src/main/java/dev/amiah/ephemeral/ui/element/ActionBar.kt
@@ -10,30 +10,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import dev.amiah.ephemeral.MainActivity
 import dev.amiah.ephemeral.R
 
 @Composable
-fun ActionBar() {
-    Row(modifier = Modifier.fillMaxWidth().padding(4.dp)) {
+fun ActionBar(navController: NavHostController) {
+    Row(modifier = Modifier.fillMaxWidth().padding(2.dp)) {
 
         Box(modifier = Modifier.weight(0.1f))
 
         // See all reminders
-        IconButton(onClick = {}) {
+        IconButton(onClick = { navController.navigate(MainActivity.ViewReminders) }) {
             Icon(
                 painterResource(R.drawable.see_reminders),
                 contentDescription = "",
             )
         }
         // See all task notes
-        IconButton(onClick = {}) {
+        IconButton(onClick = { navController.navigate(MainActivity.ViewTasks) }) {
             Icon(
                 painterResource(R.drawable.see_notes),
                 contentDescription = "",
             )
         }
         // Settings
-        IconButton(onClick = {}) {
+        IconButton(onClick = { navController.navigate(MainActivity.Settings) }) {
             Icon(
                 painterResource(R.drawable.gear),
                 contentDescription = "",
