e0af137237c02de94a114ca757e8d4349bbccb91
AmiahRaine <amiah.belleisle@proton.me>
2026-01-31 17:42:56 -0600

    Update to use Reminder Repository instead of Dao

--- app/src/main/java/dev/amiah/ephemeral/viewmodel/reminder/RemindersViewModel.kt
+++ app/src/main/java/dev/amiah/ephemeral/viewmodel/reminder/RemindersViewModel.kt
@@ -3,8 +3,8 @@ package dev.amiah.ephemeral.viewmodel.reminder
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dev.amiah.ephemeral.data.dao.ReminderDao
 import dev.amiah.ephemeral.data.entity.Reminder
+import dev.amiah.ephemeral.data.repository.RemindersRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
@@ -15,19 +15,18 @@ import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import java.time.Instant
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import javax.inject.Inject
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @HiltViewModel
-class RemindersViewModel @Inject constructor (private val reminderDao: ReminderDao) : ViewModel() {
+class RemindersViewModel @Inject constructor (private val remindersRepository: RemindersRepository) : ViewModel() {
 
     // Default ReminderState.
     private val _state = MutableStateFlow(RemindersState())
 
-    private val _reminders = reminderDao.getActiveRemindersByDate(Instant.now())
+    private val _reminders = remindersRepository.getActiveReminders()
 
     val state = combine(_state, _reminders) { state, reminders ->
         state.copy(
@@ -44,20 +43,26 @@ class RemindersViewModel @Inject constructor (private val reminderDao: ReminderD
             // Create, Delete, Save Reminders
 
             is ReminderEvent.CreateReminder -> {
-                viewModelScope.launch(Dispatchers.IO) {
-                    reminderDao.upsert(Reminder(time = ZonedDateTime.of(event.dateTime, ZoneId.systemDefault()).toInstant()))
+                viewModelScope.launch {
+                    // Upsert new reminder. Time is converted from local date time to instant
+                    remindersRepository.upsert(
+                        Reminder(time = ZonedDateTime
+                            .of(event.dateTime, ZoneId.systemDefault())
+                            .toInstant()
+                        )
+                    )
                 }
             }
 
             is ReminderEvent.DeleteReminder -> {
-                viewModelScope.launch(Dispatchers.IO) {
-                    reminderDao.delete(event.reminder)
+                viewModelScope.launch {
+                    remindersRepository.delete(event.reminder)
                 }
             }
 
             is ReminderEvent.SaveReminder -> {
-                viewModelScope.launch(Dispatchers.IO) {
-                    reminderDao.upsert(event.reminder)
+                viewModelScope.launch {
+                    remindersRepository.upsert(event.reminder)
                 }
             }
 
@@ -70,7 +75,7 @@ class RemindersViewModel @Inject constructor (private val reminderDao: ReminderD
                     // Give time for job to be canceled
                     delay(120)
                     // If no new job comes in, do the save
-                    reminderDao.upsert(event.reminder)
+                    remindersRepository.upsert(event.reminder)
                 }
             }
 
