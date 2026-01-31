d6d144ac98606de207758b70ec3d7238631f832a
AmiahRaine <amiah.belleisle@proton.me>
2026-01-31 17:45:40 -0600

    Added companion object with method that creates work

--- app/src/main/java/dev/amiah/ephemeral/domain/worker/DatabaseManagementWorker.kt
+++ app/src/main/java/dev/amiah/ephemeral/domain/worker/DatabaseManagementWorker.kt
@@ -1,9 +1,14 @@
 package dev.amiah.ephemeral.domain.worker
 
 import android.content.Context
+import androidx.work.Constraints
 import androidx.work.CoroutineWorker
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import dev.amiah.ephemeral.domain.DatabaseManager
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class DatabaseManagementWorker(
@@ -19,6 +24,30 @@ class DatabaseManagementWorker(
         dbMan.manageInactiveNotes()
 
         return Result.success()
+
+    }
+
+    companion object {
+
+        /**
+         * Creates and enqueues periodic work to manage the database.
+         * This work starts immediately and replaces the previous worker.
+         */
+        fun startDatabaseWork(appContext: Context) {
+            val databaseManagementPeriodicWorkRequest =
+                PeriodicWorkRequestBuilder<DatabaseManagementWorker>(
+                    repeatInterval = 2,
+                    repeatIntervalTimeUnit = TimeUnit.HOURS,
+                )
+                    .setConstraints(Constraints(requiresDeviceIdle = true))
+                    .build()
+
+            WorkManager.getInstance(appContext).enqueueUniquePeriodicWork(
+                uniqueWorkName = "ephemeral.manage.data",
+                existingPeriodicWorkPolicy = ExistingPeriodicWorkPolicy.CANCEL_AND_REENQUEUE,
+                request = databaseManagementPeriodicWorkRequest
+            )
+        }
 
     }
 
